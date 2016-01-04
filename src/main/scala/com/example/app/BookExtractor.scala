50e4b84c999bd0e007d8f6eb0a226fe017b9ff3c
vinhbachsy <bsdvinhuit@gmail.com>
2016-01-04 23:50:20 +0800

    Implement basic book detail HTML extractor

--- src/main/scala/com/example/app/BookExtractor.scala
+++ src/main/scala/com/example/app/BookExtractor.scala
@@ -1,10 +1,27 @@
 package com.example.app
 
-import org.jsoup.nodes.Document
+import org.jsoup.nodes.{Element, Document}
+
+import scala.collection.JavaConverters._
+import scala.collection.immutable.IndexedSeq
 
 /**
   * Created by vinhbachsy on 1/1/16.
   */
 object BookExtractor {
-  def extract(doc: Document, config: Map[String, String]): Option[List[Map[String, Any]]] = Some(Nil)
+  def extract(doc: Document, config: Map[String, String]): Option[List[Map[String, Any]]] = {
+    val items = doc.select(config.getOrElse("item", ""))
+    if (items.isEmpty) return None
+    val results = for {
+      index <- 0 until items.size()
+      item <- items.get(index)
+    } yield extractItem(item, config)
+
+    Some(results)
+  }
+
+  private def extractItem(item: Element, config: Map[String, String]): Map[String, Any] = {
+    item.select(config.getOrElse("title", ""))
+    Map("title" -> "", "author" -> "123")
+  }
 }
--- src/test/scala/com/example/app/BookExtractorSpec.scala
+++ src/test/scala/com/example/app/BookExtractorSpec.scala
@@ -11,9 +11,21 @@ import scala.io.Source
   */
 class BookExtractorSpec extends FlatSpec with Matchers {
   it should "return list of items if selector exists" in {
-    val config = Map("item" -> "", "title" -> "", "author" -> "", "image" -> "", "link" -> "", "price" -> "")
+    val config = Map(
+      "item" -> ".product-item",
+      "title" -> "> a[title]",
+      "author" -> ".author",
+      "image" -> ".image > img[src]",
+      "link" -> "> a[href]",
+      "price" -> ".price-sale")
     val html = Source.fromURL(getClass.getResource("/tiki.html")).getLines mkString "\n"
     val doc = Jsoup.parse(html)
-    assert(BookExtractor.extract(doc, config) == Some(Nil))
+    val results = BookExtractor.extract(doc, config).getOrElse(Nil)
+    assert(results.size == 4)
+    assert(results.map(_.getOrElse("title", "")) ==
+      List("Tôi Thấy Hoa Vàng Trên Cỏ Xanh (Nguyễn...",
+        "Combo Tôi Thấy Hoa Vàng Trên Cỏ Xanh",
+        "Tôi Thấy Hoa Vàng Trên Cỏ Xanh",
+        "Tôi Thấy Hoa Vàng Trên Cỏ Xanh (Nguyễn Nhật Ánh)"))
   }
 }
